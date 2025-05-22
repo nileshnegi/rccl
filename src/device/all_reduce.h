023ac9e175dbf25ba05f89b92ad8ecd394bccf89
nileshnegi <Nilesh.Negi@amd.com>
2025-05-22 14:12:23 +0000

    [DEVICE] Enable one-slice for gfx950
    Signed-off-by: nileshnegi <Nilesh.Negi@amd.com>

--- src/device/all_reduce.h
+++ src/device/all_reduce.h
@@ -558,7 +558,7 @@ namespace {
   }
 }
 
-#ifdef __gfx942__ // Use a single slice for a single node on gfx942 (MI300X, MI300A, MI325).  Otherwise, use the default.
+#if defined(__gfx942__) || defined(__gfx950__) // Use a single slice for a single node on gfx942 and gfx950. Otherwise, use the default.
 #define rcclAllReduceRunRingSimpleProtoImpl(tid, nthreads, work) \
   if(work->oneNode){ \
     using Proto = ProtoSimple<ALLREDUCE_CHUNKSTEPS/ALLREDUCE_SLICESTEPS_SINGLE_NODE, ALLREDUCE_SLICESTEPS_SINGLE_NODE>; \
