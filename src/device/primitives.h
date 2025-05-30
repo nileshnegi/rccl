60f18e5970f834fed270f29184c2e8e8d42cb402
nileshnegi <Nilesh.Negi@amd.com>
2025-05-30 05:47:26 +0000

    [DEVICE] Use threadfence on gfx950 for LL protocol
    Signed-off-by: nileshnegi <Nilesh.Negi@amd.com>

--- src/device/primitives.h
+++ src/device/primitives.h
@@ -15,21 +15,47 @@
 
 #define NCCL_SPINS_BEFORE_CHECK_ABORT 1000000
 
-#if defined(__gfx942__) || defined(__gfx950__)
-#define __THREAD_FENCE __threadfence_block()
-#else
-#define __THREAD_FENCE __threadfence()
-#endif
-
 #define barrier_by_group() do { \
   if (nthreads == NCCL_MAX_NTHREADS) { \
-    __THREAD_FENCE; __builtin_amdgcn_s_barrier(); \
+    __threadfence() ; __builtin_amdgcn_s_barrier(); \
   } else { \
     const int w = threadIdx.x/WARP_SIZE; \
     const int wid = threadIdx.x%WARP_SIZE; \
     if (wid == 0) { \
       barrier_next += nthreads/WARP_SIZE; \
-      __THREAD_FENCE; \
+      __threadfence(); \
+      __hip_atomic_fetch_add(barriers, 1, __ATOMIC_RELEASE, __HIP_MEMORY_SCOPE_WORKGROUP); \
+      int spins = 0; \
+      int rate_limit = 50; \
+      while (__hip_atomic_load(barriers, __ATOMIC_ACQUIRE, __HIP_MEMORY_SCOPE_WORKGROUP) < barrier_next) { \
+        spins++; \
+        if (spins == NCCL_SPINS_BEFORE_CHECK_ABORT) { \
+          if (__atomic_load_n(ncclShmem.comm.abortFlag, __ATOMIC_SEQ_CST)) { \
+            ncclShmem.aborted = 1; \
+            break; \
+          } \
+          spins = 0; \
+        } \
+        if (spins == 0 && rate_limit > 0) { \
+          rate_limit --; \
+          traceData(__LINE__, threadIdx.x, __hip_atomic_load(barriers, __ATOMIC_ACQUIRE, __HIP_MEMORY_SCOPE_WORKGROUP), barrier_next); \
+        } \
+        __builtin_amdgcn_s_sleep(1); \
+      } \
+      __asm__ __volatile__("s_wakeup"); \
+    } \
+  } \
+} while (0)
+
+#define barrier_by_group_block() do { \
+  if (nthreads == NCCL_MAX_NTHREADS) { \
+    __threadfence_block(); __builtin_amdgcn_s_barrier(); \
+  } else { \
+    const int w = threadIdx.x/WARP_SIZE; \
+    const int wid = threadIdx.x%WARP_SIZE; \
+    if (wid == 0) { \
+      barrier_next += nthreads/WARP_SIZE; \
+      __threadfence_block(); \
       __hip_atomic_fetch_add(barriers, 1, __ATOMIC_RELEASE, __HIP_MEMORY_SCOPE_WORKGROUP); \
       int spins = 0; \
       int rate_limit = 50; \
