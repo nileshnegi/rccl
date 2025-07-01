3b96fa4f15a8c06f57d593a59bb64cd849f74612
Nilesh M Negi <Nilesh.Negi@amd.com>
2025-06-30 20:13:35 -0500

    [AzureCI] Enable rccl-tests and pytorch-tests
    Temp. reducing RCCL UT coverage

--- .azuredevops/slurm/build.sh
+++ .azuredevops/slurm/build.sh
@@ -28,9 +28,26 @@ fi
 echo "Using Ninja at: $(which ninja)"
 ninja --version
 
+# Define GPU target
+export GPU_TARGETS="gfx942"
+
 cd "${SLURM_SUBMIT_DIR:-$PWD}"
 mkdir -p build
 cd build
-cmake -G Ninja -DCMAKE_INSTALL_PREFIX="$BINARIES_DIR" -DCMAKE_BUILD_TYPE=Release -DGPU_TARGETS=gfx942 -DBUILD_TESTS=ON -DROCM_PATH="$ROCM_PATH" ..
+cmake -G Ninja -DCMAKE_INSTALL_PREFIX="$BINARIES_DIR" -DCMAKE_BUILD_TYPE=Release -DGPU_TARGETS=${GPU_TARGETS} -DBUILD_TESTS=ON -DROCM_PATH="$ROCM_PATH" ..
 cmake --build .
 cmake --build . --target install
+
+cd "${SLURM_SUBMIT_DIR:-$PWD}"
+git clone https://github.com/ROCm/rccl-tests
+cd rccl-tests
+mkdir -p build
+cd build
+cmake -DCMAKE_PREFIX_PATH="$BINARIES_DIR;$MPI_HOME" -DUSE_MPI=ON -DCMAKE_INSTALL_PREFIX="$BINARIES_DIR" -DCMAKE_BUILD_TYPE=Release -DGPU_TARGETS=${GPU_TARGETS} -DROCM_PATH="$ROCM_PATH" ..
+cmake --build .
+cmake --build . --target install
+
+cd "${BINARIES_DIR}"
+mkdir -p pytorch-tests
+cp /mnt/GT_NFS/new_gt/nilenegi/pytorch-tests/run_pytorch_tests.sh "${BINARIES_DIR}"/pytorch-tests/run_pytorch_tests.sh
+
--- .azuredevops/slurm/test_rccl-UnitTests.sh
+++ .azuredevops/slurm/test_rccl-UnitTests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=rccl-test
-#SBATCH --output=rccl-test-%j.out
-#SBATCH --error=rccl-test-%j.out
+#SBATCH --output=%x-%j.out
+#SBATCH --error=%x-%j.out
 #SBATCH --time=120
 #SBATCH --nodes=1
 #SBATCH --exclusive
@@ -13,4 +13,4 @@ echo "Node identifier: $short_id"
 source /etc/profile.d/lmod.sh
 module load rocm/6.2.0
 cd "$BINARIES_DIR/bin"
-LD_LIBRARY_PATH="$BINARIES_DIR/lib:$LD_LIBRARY_PATH" NCCL_DEBUG=INFO RCCL_ENABLE_SIGNALHANDLER=1 ./rccl-UnitTests --gtest_output=xml:$PIPELINE_WORKSPACE/test_output.xml --gtest_color=yes
+LD_LIBRARY_PATH="$BINARIES_DIR/lib:$LD_LIBRARY_PATH" NCCL_DEBUG=INFO RCCL_ENABLE_SIGNALHANDLER=1 ./rccl-UnitTests --gtest_filter="StandaloneTests.*" --gtest_output=xml:$PIPELINE_WORKSPACE/rccl-UnitTests_output.xml --gtest_color=yes
